f8c0f0cebc9b0dfd21fda7ad126f3ab5f2263a17
Maks125x <MAKS125X@yandex.ru>
2024-02-21 13:42:05 +0400

    Изменено переопределение equals() в Book.kt

--- app/src/main/java/com/example/simbirsoftmobile/kotlin/part1/Book.kt
+++ app/src/main/java/com/example/simbirsoftmobile/kotlin/part1/Book.kt
@@ -1,7 +1,5 @@
 package com.example.simbirsoftmobile.kotlin.part1
 
-import kotlin.jvm.Throws
-
 class Book(
     override val price: Int,
     override val wordCount: Int
@@ -19,8 +17,21 @@ class Book(
         else -> throw IllegalStateException("$wordCount меньше единицы")
     }
 
-    override fun equals(other: Any?): Boolean =
-        if (other is Book) price == other.price && wordCount == other.wordCount else false
+    override fun equals(other: Any?): Boolean {
+        if (other === this) {
+            return true
+        }
+
+        if (other !is Book) {
+            return false
+        }
+
+        if (other.hashCode() != this.hashCode()) {
+            return false
+        }
+
+        return price == other.price && wordCount == other.wordCount
+    }
 
     override fun hashCode(): Int {
         var result = price
